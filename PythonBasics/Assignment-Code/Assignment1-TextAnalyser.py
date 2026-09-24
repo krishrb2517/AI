d9e52be673cb80d496b20821b96572dd58939864
Krish <krish.rb.2517@gmail.com>
2026-09-24 20:09:46 +0530

    Downloaded the book and wrote the code to take the path of book as an argument and read it to process further

---
+++ PythonBasics/Assignment-Code/Assignment1-TextAnalyser.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Enter the path of text to analyse.")
+parser.add_argument("--path")
+args = parser.parse_args()
+
+if args.path:
+    print(args.path)
+
+path = args.path
+
